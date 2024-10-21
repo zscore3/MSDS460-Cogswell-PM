ff7262bdbea3e59a2aef9c0a10ac0e5169fb2027
zscore3 <60632046+zscore3@users.noreply.github.com>
2024-10-20 21:27:12 -0400

    Update Cogswell_Code.py

--- Cogswell_Code.py
+++ Cogswell_Code.py
@@ -1,23 +1,55 @@
 # Critical Path Analysis 
 
-# Problem description from Williams (2013, pages 95-98)
-# Williams, H. Paul. 2013. Model Building in Mathematical Programming (fifth edition). New York: Wiley. [ISBN-13: 978-1-118-44333-0]
+# Adapted from the following Sources:  
+## Problem description from Williams (2013, pages 95-98)
+## Williams, H. Paul. 2013. Model Building in Mathematical Programming (fifth edition). New York: Wiley. [ISBN-13: 978-1-118-44333-0]
 
-# Python PuLP solution prepared by Thomas W. Miller
-# Revised April 20, 2023
-# Implemented using activities dictionary with derived start_times and end_times
-# rather than time decision variables as in Williams (2013)
+## Python PuLP solution prepared by Thomas W. Miller
+## Revised April 20, 2023
+## Implemented using activities dictionary with derived start_times and end_times
+## rather than time decision variables as in Williams (2013)
+
+##Printing to text file provided from GeeksforGeeks (https://www.geeksforgeeks.org/how-to-fix-permissionerror-errno-13-permission-denied-in-python/)
+##daswanta_kumar_routhu (2024)
 
 from pulp import *
 
 # Create a dictionary of the activities and their durations
-activities = {'DigFoundations':4, 'LayFoundations':2, 'ObtainBricks':7, 'ObtainTiles':12, 'Walls':10, 'Roofing':5, 'Wiring':3, 'Painting':4}
+activities = {'A':2, 
+              'B':2, 
+              'C':2, 
+              'D1':2, 
+              'D2':2, 
+              'D3':2, 
+              'D4':2, 
+              'D5':2, 
+              'D6':2, 
+              'D7':2, 
+              'D8':2, 
+              'E':2, 
+              'F':2, 
+              'G':2, 
+              'H':2}
 
 # Create a list of the activities
 activities_list = list(activities.keys())
 
 # Create a dictionary of the activity precedences
-precedences = {'DigFoundations': [], 'ObtainBricks': [], 'ObtainTiles': [], 'LayFoundations': ['DigFoundations'],  'Walls': ['DigFoundations','ObtainBricks'], 'Wiring': ['Walls'], 'Roofing': ['ObtainTiles','Walls'], 'Painting': ['Wiring','Roofing']}
+precedences = {'A': [], 
+               'B': [], 
+               'C': ['A'], 
+               'D1': ['A'],  
+               'D2': ['D1'], 
+               'D3': ['D1'], 
+               'D4': ['D2','D3'], 
+               'D5': ['D4'], 
+               'D6': ['D4'], 
+               'D7': ['D6'], 
+               'D8': ['D5','D7'], 
+               'E': ['B','C'], 
+               'F': ['D8','E'],
+               'G': ['A', 'D8'],
+               'H': ['F', 'G']}
 
 # Create the LP problem
 prob = LpProblem("Critical Path", LpMinimize)
@@ -44,10 +76,31 @@ for activity in activities_list:
     if value(start_times[activity]) == 0:
         print(f"{activity} starts at time 0")
     if value(end_times[activity]) == max([value(end_times[activity]) for activity in activities_list]):
-        print(f"{activity} ends at {value(end_times[activity])} days in duration")
+        print(f"{activity} ends at {value(end_times[activity])} weeks in duration")
 
 # Print solution
 print("\nSolution variable values:")
 for var in prob.variables():
     if var.name != "_dummy":
         print(var.name, "=", var.varValue)
+
+file_path = r"C:\Users\jrcog\Downloads\output.txt"
+
+with open(file_path, "w") as file:
+    file.write("Critical Path time:")
+    for activity in activities_list:
+        if value(start_times[activity]) == 0:
+            file.write(f"\n{activity} starts at time 0")
+        if value(end_times[activity]) == max([value(end_times[activity]) for activity in activities_list]):
+            file.write(f"\n{activity} ends at {value(end_times[activity])} weeks in duration")
+            
+        # Print solution
+    file.write("\nSolution variable values:\n")
+    for var in prob.variables():
+        if var.name != "_dummy":
+            file.write(var.name)
+            file.write("=")
+            file.write(f"{var.varValue}")
+            file.write('\n')
+
+print("File content modified successfully!")
